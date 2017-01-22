019cf1f15885c98280e0af715a42abdebd795211
Fattaneh2016 <fatameri@hotmail.com>
2017-01-22 00:00:59 -0500

    Add project files.

---
+++ Grades/GradeBook.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Grades
+{
+    class GradeBook
+    {
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        public void Removegrade(int id)
+        {
+            grades.Remove(id);
+        }
+
+        List<float> grades = new List<float>();
+    }
+}
---
+++ Grades/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(45);
+            book.AddGrade(87.9f);
+            book.AddGrade(6.8f);
+            book.AddGrade(9.8f);
+        }
+    }
+}
