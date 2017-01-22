ba0ba0cdc9a01b8799dbb748c7b088aa1f8895b6
Fattaneh2016 <fatameri@hotmail.com>
2017-01-22 13:20:12 -0500

    add unit tesst

--- Grades/GradeStatistics.cs
+++ Grades/GradeStatistics.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Grades
+﻿namespace Grades
 {
     class GradeStatistics
     {
@@ -13,9 +7,9 @@ namespace Grades
             HighestGrade = 0;
             Lowestgrade = float.MaxValue;
         }
-         public float AverageGrade;
-         public float Lowestgrade;
-         public float HighestGrade;
+        public float AverageGrade;
+        public float Lowestgrade;
+        public float HighestGrade;
 
     }
 }
