07ce36ce428cda729c2d88c31c71e96fe35ea21e
Fattaneh2016 <fatameri@hotmail.com>
2017-01-22 13:53:25 -0500

    unit test computeGrade

---
+++ Grades.Tests/GradeBookTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Grades.Tests
+{
+    [TestClass]
+    public class GradeBookTests
+    {
+        [TestMethod]
+        public void ComputeHighestGrade()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(56);
+            book.AddGrade(24);
+
+            GradeStatistics result = book.ComputeStatistics();
+            Assert.AreEqual(56, result.HighestGrade);
+
+        }
+
+
+    }
+}
